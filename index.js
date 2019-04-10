0d67e5a1d7c0fa93b74702725280e0ac0beb829e
alex <alex@alexs-MacBook-Pro.local>
2019-04-09 19:16:54 -0500

    changes minors

--- index.js
+++ index.js
@@ -1,22 +1,19 @@
 const express = require("express");
-// const favicon = require('express-favicon');
 const app = express();
 
 const fs = require("fs");
-const path = require("path"); // replace const path = require('path');
+const path = require("path"); 
 
-// the __dirname is the current directory from where the script is running
-// app.use(favicon(__dirname + '/build/favicon.ico'));
-/*
-app.get('/favicon.ico', (req,res) => {
-    res.end()
-})
-*/
+// proccess.env es una configuración de variable de entorno
+// heroku asigna un puerto entonces 
+// en prod se usa la variable de entorno
+// en dev se usa el puerto asignado
+const port = process.env.PORT || 8080;
 
 // all posts
 app.get("/posts", (req, res) => {
   const posts = [];
-  fs.readdir("posts", (err, files) => {
+  fs.readdir("posts", (err, files) => { // leemos el directorio
     if (err) throw err;
     files.forEach(file => {
       // Asynchronously reads the entire contents of a file.
@@ -55,23 +52,4 @@ app.get("/posts/:id", function(req, res) {
   });
 });
 
-const server = app.listen(5000, () => {
-  console.log(`Server runing at http://localhost: ${server.address().port}`);
-});
-
-//server.js
-// configuración para servir la aplicacion de react con myJsonserver
-/*
-const port = process.env.PORT || 8080;
-
-const app = express();
-app.use(express.static(__dirname));
-app.use(express.static(path.join(__dirname, 'build')));
-app.get('/ping', function (req, res) {
- return res.send('pong');
-});
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
-app.listen(port);
-*/
+const server = app.listen(port)
